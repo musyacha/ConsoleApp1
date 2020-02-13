b3abda215fdac77d8dd82228c0a14c17ef0885be
MATH RUDN <rudn.math@outlook.com>
2020-02-13 15:46:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello WODLD");
+            //Console.ReadLine();
+            Console.Write("Введите>");
+            String str = Console.ReadLine();
+            Console.WriteLine("Введено " + str);
+            Console.ReadLine();
+
+        }
+    }
+}
